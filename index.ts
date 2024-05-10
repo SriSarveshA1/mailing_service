477b1179ff7b8d8fb7c087c52a8550f2aca05b67
srisarvesh <srisarvesh@peppo.co.in>
2024-05-10 07:23:03 +0530

    Initial project setup done

---
+++ index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+const app = express();
+app.use(express.json());
+
+
+
+
+const port = process.env.PORT || 5000;
+app.listen(port,()=>{
+    console.log("listening on port " + port);
+});
